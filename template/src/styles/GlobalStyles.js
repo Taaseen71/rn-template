3277e6ef415f67558110cda824a0ef5172bb6965
Saadat Taaseen <login@saadt.dev>
2024-02-13 11:43:15 -0500

    Update GlobalStyles.js

--- template/src/styles/GlobalStyles.js
+++ template/src/styles/GlobalStyles.js
@@ -14,7 +14,7 @@ const globalStyle = (arg1, arg2, arg3, arg4) => StyleSheet.create({
         backgroundColor: 'black'
     },
     TextInputComponent:{
-        height: 40,
+        height: !arg1 ? 40 : arg1,
         margin: 12,
         borderWidth: 1,
         padding: 10,
@@ -22,6 +22,36 @@ const globalStyle = (arg1, arg2, arg3, arg4) => StyleSheet.create({
     },
     bcolor: {
         backgroundColor: arg1
+    },
+    inline: {
+        flexDirection: 'row',
+        justifyContent: arg1 ? arg1 : 'space-between',
+        // backgroundColor: 'yellow',
+        margin: 12,
+        alignItems: 'center'
+    },
+    multiline: {
+        flexDirection: 'column',
+        justifyContent: arg1 ? arg1 : 'space-between',
+        backgroundColor: arg2 ? arg2 :  'none',
+        marginLeft: 12
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: arg1 ? arg1  : '#DDDDDD',
+        padding: 10,
+        margin: 12
+      },
+    Image: {
+        width: arg1 ? arg1 : 200,
+        height: arg2 ? arg2 : 200,
+        margin: 12,
+    },
+    borders: {
+        borderWidth: 1,
+        padding: 10,
+        margin: 12,
+        borderRadius: 6
     }
 
 })
